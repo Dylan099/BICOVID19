5855e2148298784bf134935fe324cadadb325e0e
Dylan099 <you@example.com>
2020-06-07 20:48:59 -0400

    CREACION DEL PROYECTO

---
+++ BusinessInteligenceCOVID19Bolivia/src/main/java/com/example/BusinessInteligenceCOVID19Bolivia/BusinessInteligenceCovid19BoliviaApplication.java
@@ -0,0 +1,13 @@
+package com.example.BusinessInteligenceCOVID19Bolivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessInteligenceCovid19BoliviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessInteligenceCovid19BoliviaApplication.class, args);
+	}
+
+}
---
+++ BusinessInteligenceCOVID19Bolivia/src/test/java/com/example/BusinessInteligenceCOVID19Bolivia/BusinessInteligenceCovid19BoliviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusinessInteligenceCOVID19Bolivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessInteligenceCovid19BoliviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
